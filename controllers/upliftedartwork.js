e7c76384eb36a2d8145b71da5984983925c35712
Yash Agrawal <yashagrawal9265@gmail.com>
2024-04-05 20:20:25 -0500

    add put request

--- controllers/upliftedartwork.js
+++ controllers/upliftedartwork.js
@@ -26,14 +26,21 @@ exports.upliftedartwork_view_all_Page = async function (req, res) {
 };
 
 // for a specific upliftedartwork.
-exports.upliftedartwork_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: upliftedartwork detail: " + req.params.id);
+exports.upliftedartwork_detail = async function (req, res) {
+  console.log("detail" + req.params.id);
+  try {
+    result = await Upliftedartwork.findById(req.params.id);
+    res.send(result);
+  } catch (error) {
+    res.status(500);
+    res.send(`{"error": document for id ${req.params.id} not found`);
+  }
 };
 
 // Handle upliftedartwork create on POST.
 exports.upliftedartwork_create_post = async function (req, res) {
   console.log(req.body);
-  let document = new Upliftedartwork    ();
+  let document = new Upliftedartwork();
   // We are looking for a body, since POST does not have query parameters.
   // Even though bodies can be in many different formats, we will be picky
   // and require that it be a json object
@@ -55,7 +62,23 @@ exports.upliftedartwork_create_post = async function (req, res) {
 exports.upliftedartwork_delete = function (req, res) {
   res.send("NOT IMPLEMENTED: upliftedartwork delete DELETE " + req.params.id);
 };
+
 // Handle upliftedartwork update form on PUT.
-exports.upliftedartwork_update_put = function (req, res) {
-  res.send("NOT IMPLEMENTED: upliftedartwork update PUT" + req.params.id);
+exports.upliftedartwork_update_put = async function (req, res) {
+  console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`);
+  try {
+    let toUpdate = await Upliftedartwork.findById(req.params.id);
+    // Do updates of properties
+    if (req.body.material) toUpdate.material = req.body.material;
+    if (req.body.style) toUpdate.style = req.body.style;
+    if (req.body.price) toUpdate.price = req.body.price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result);
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+  }
 };
--- routes/upliftedartwork.js
+++ routes/upliftedartwork.js
@@ -4,4 +4,7 @@ var router = express.Router();
 
 router.get('/', upliftedartwork_controlers.upliftedartwork_view_all_Page );
 
+// GET request for one upliftedartwork.
+router.get('/upliftedartworks/:id', upliftedartwork_controlers.upliftedartwork_detail);
+
 module.exports = router;
